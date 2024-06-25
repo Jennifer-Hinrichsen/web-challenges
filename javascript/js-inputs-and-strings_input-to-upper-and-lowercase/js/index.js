2998f870fd5cb076aea077c7d5297b73db0cd5e4
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-06-25 09:35:56 +0200

    added solution for challenge 2

--- javascript/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
+++ javascript/js-inputs-and-strings_input-to-upper-and-lowercase/js/index.js
@@ -10,11 +10,35 @@ Convert the input value to lowercase when clicking a second button
 
 Hint: Access the value by using `.value` on the input element
 
-*/
+
 
 const input = document.querySelector('[data-js="first-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 
 uppercaseButton.addEventListener("click", () => {
   input.value = input.value.toUpperCase();
+});
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Assign the input element to a variable
+  const input = document.querySelector('[data-js="first-input"]');
+
+  // Assign the uppercase button element to a variable
+  const uppercaseButton = document.querySelector(
+    '[data-js="button-uppercase"]'
+  );
+
+  // Assign the lowercase button element to a variable
+  const lowercaseButton = document.querySelector(
+    '[data-js="button-lowercase"]'
+  );
+
+  uppercaseButton.addEventListener("click", () => {
+    input.value = input.value.toUpperCase();
+  });
+
+  lowercaseButton.addEventListener("click", () => {
+    input.value = input.value.toLowerCase();
+  });
 });
