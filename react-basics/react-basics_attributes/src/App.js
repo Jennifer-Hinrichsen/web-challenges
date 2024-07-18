2a8962597668fa309b3eec3d21c56a79367663a4
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-07-18 11:25:29 +0200

    added solution 3

--- react-basics/react-basics_attributes/src/App.js
+++ react-basics/react-basics_attributes/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello Again</h2>
+      <label htmlFor="subscribe">Subscribe</label>
+      <input type="email" id="subscribe" name="subscribe" />
+      <a className="article__link" href="https://de.wikipedia.org/wiki/React">
+        Read more on Wikipedia
+      </a>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
 }
