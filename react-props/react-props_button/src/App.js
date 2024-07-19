990ef4f7f3eccfe408673a8c365d9ad1addce03d
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-07-19 11:49:10 +0200

    added src/App.js

--- react-props/react-props_button/src/App.js
+++ react-props/react-props_button/src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
 import "./styles.css";
-
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Button color="blue" text="Click me" />;
+}
+function Button({ color, disabled, text }) {
+  return (
+    <button style={{ height: "100px" }} disabled={disabled}>
+      {text}
+    </button>
+  );
 }
