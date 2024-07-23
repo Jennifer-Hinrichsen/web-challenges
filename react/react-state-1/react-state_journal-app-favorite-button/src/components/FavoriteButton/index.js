19e6293883ff5b3ed8b157495ab95517090a4039
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-07-23 12:02:35 +0200

    added package

--- react/react-state-1/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
+++ react/react-state-1/react-state_journal-app-favorite-button/src/components/FavoriteButton/index.js
@@ -1,20 +1,45 @@
 import "./FavoriteButton.css";
+import React, { useState } from "react";
 import { ReactComponent as StarFilled } from "./star-filled.svg";
 import { ReactComponent as Star } from "./star.svg";
 
 export default function FavoriteButton() {
   // this should be a state variable
-  const isFavorite = false;
+  const [isFavorite, setFavorite] = useState(false);
 
+  const toggleFavorite = () => {
+    setFavorite(!isFavorite);
+  };
   return (
     <button
       className="favorite-button"
-      onClick={() => {
-        console.log("favorite button clicked");
-      }}
+      onClick={toggleFavorite}
       aria-label="favorite"
     >
       {isFavorite ? <StarFilled /> : <Star />}
     </button>
   );
 }
+
+//other posibble solution
+//import { useState } from "react";
+//import "./FavoriteButton.css";
+//import { ReactComponent as StarFilled } from "./star-filled.svg";
+//import { ReactComponent as Star } from "./star.svg";
+//export default function FavoriteButton() {
+// this should be a state variable
+// const [isFavorite, setIsFavourite] = useState(false);
+//  return (
+//    <button
+//      className="favorite-button"
+//      onClick={() => {
+//        setIsFavourite(!isFavorite);
+//        console.log("favourite Button clicked:", !isFavorite);
+//      }}
+//      aria-label="favorite"
+//   >
+//      {isFavorite ? <StarFilled /> : <Star />}
+//    </button>
+//  );
+//}
+//
