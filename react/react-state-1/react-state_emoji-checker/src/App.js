2db73a7c6f3207bbae1615832540b8bd496bdd23
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-07-23 11:41:52 +0200

    added solution 3

--- react/react-state-1/react-state_emoji-checker/src/App.js
+++ react/react-state-1/react-state_emoji-checker/src/App.js
@@ -1,52 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let code = "?";
-
+  const [code, setCode] = useState("");
   const validCode = "🐡🐠🐋";
+
+  const handleClick = (event) => {
+    setCode(code + event.target.textContent);
+  };
+  console.log(handleClick);
+  const handleReset = () => {
+    setCode("");
+  };
 
   return (
     <div className="container">
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
         </button>{" "}
       </div>
 
-      <button
-        type="button"
-        onClick={() => {
-          console.log("Reset Code!");
-        }}
-      >
+      <button type="button" onClick={handleReset}>
         Reset
       </button>
 
