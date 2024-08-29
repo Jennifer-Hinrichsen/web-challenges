5ce63c21220835ee2095f4272fcef8b005bcfe9f
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-08-29 13:26:33 +0200

    reworked solution

--- next-js/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/index.js
+++ next-js/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/index.js
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import { volumes } from "../../lib/data";
 
-export default function HomePage() {
+export default function VolumesOverview() {
   return (
     <div>
-      <Link href="/volumes/Movies.js">Go to all volumes</Link>
+      <h1>Lord of the Rings Volumes</h1>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/movies/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
