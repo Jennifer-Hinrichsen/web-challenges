30b48a5080c7f57f1653f5bedb82ad969a6de864
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-09-17 11:44:28 +0200

    add solution

--- backend-read/backend-read_products/pages/api/products/[id].js
+++ backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,13 +1,24 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  if (request.method === "GET") {
+    try {
+      const product = await Product.findById(id);
 
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+      if (!product) {
+        return response.status(404).json({ message: "Product not found" });
+      }
+
+      return response.status(200).json(product);
+    } catch (error) {
+      return response.status(500).json({ error: "Failed to fetch product" });
+    }
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
   }
-
-  response.status(200).json(product);
 }
