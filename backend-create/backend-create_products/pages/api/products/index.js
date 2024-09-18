5e24ce4f37940770655f64a91498da17c3d0c0e1
Jennifer-Hinrichsen <hinrichsen.jennifer@gmx.net>
2024-09-18 11:12:12 +0200

    add error boundary

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -5,8 +5,13 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const products = await Product.find();
-    return response.status(200).json(products);
+    try {
+      const products = await Product.find();
+      return response.status(200).json(products);
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ error: "Failed to fetch products." });
+    }
   }
 
   if (request.method === "POST") {
